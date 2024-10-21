473baa1ade4d18fad49a6b4f89f6d9ec5d4ec08f
Belithe <JunkmailBelith@gmail.com>
2024-10-21 20:02:38 +0200

    Updated reader files with basic elements from writer

---
+++ static/scripts/reader.js
@@ -0,0 +1,8 @@
+function toggleActive(element) {
+    var nav = document.getElementById("formNav")
+    var images = nav.querySelectorAll("img")
+    images.forEach(img => {
+        img.classList.remove("active")
+    });
+    element.classList.add("active")
+}
