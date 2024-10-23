7ee72eedfb26abd8a9f1e18732d5be60aa69b04a
Jordy <jordyschifferstein@hotmail.com>
2024-10-23 19:01:18 +0200

    Added modal header styling and animation

--- static/scripts/writer.js
+++ static/scripts/writer.js
@@ -113,3 +113,44 @@ function toggleKeyGlow(glowId) {
         glow.style.visibility = 'visible';
     }
 }
+
+// Modal functions
+const openModalButtons = document.querySelectorAll('.open-modal-btn');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-target');
+        let modal = document.getElementById(modalId)
+        modal.style.display = 'flex';
+        setTimeout(() => {
+            modal.classList.add('active');
+        }, 10);
+        document.body.classList.add('modal-open');
+    });
+});
+
+const closeModalButtons = document.querySelectorAll('.close-modal-btn');
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-close');
+        let modal = document.getElementById(modalId)
+        modal.classList.remove('active');
+        document.body.classList.remove('modal-open');
+
+        setTimeout(() => {
+            modal.style.display = 'none'; // Set display to none after fade out
+        }, 400);
+    });
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal')) {
+        e.target.classList.remove('active');
+        document.body.classList.remove('modal-open');
+
+        setTimeout(() => {
+            e.target.style.display = 'none'; // Set display to none after fade out
+        }, 400);
+    }
+});
