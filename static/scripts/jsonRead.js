5c545e83b0a4dce52ee89b5ebc222ae86e00a712
Belithe <JunkmailBelith@gmail.com>
2024-11-26 19:59:11 +0100

    Json reading mostly finished, added players, health, gold and characters besides ironclad

--- static/scripts/jsonRead.js
+++ static/scripts/jsonRead.js
@@ -49,11 +49,14 @@ function jsonParse(event) {
         let stringFile = event.target.result;
         let jsonFile = JSON.parse(stringFile);
 
-        //console.log(stringFile);
-
         displayGeneral(jsonFile.General);
+        displayPlayer(jsonFile.Ironclad, "ironclad");
+        displayPlayer(jsonFile.Silent, "silent");
+        displayPlayer(jsonFile.Defect, "defect");
+        displayPlayer(jsonFile.Watcher, "watcher");
 
-    } catch {
+
+    } catch (error) {
         console.log("Error parsing file. Are you sure the file is in the right format?")
         console.log(error);
     }
@@ -73,17 +76,42 @@ function displayGeneral(generalObject) {
         }
     }); 
 
+    //GENERAL
     //Fill relic lists
     fillItemList("relics", generalObject.SkippedRelics, document.getElementById("skippedRelicsBody"));
     fillItemList("boss_relics", generalObject.SkippedBossRelics, document.getElementById("skippedBossRelicsBody"));
 
-    //Fille potions and cards
+    //Fill potions and cards
     fillCardList("potions", generalObject.SkippedPotions, document.getElementById("skippedPotionsBody"));
     fillCardList("curses", generalObject.SkippedCurses, document.getElementById("skippedCursesBody"));
     fillCardList("colorless", generalObject.SkippedColorless, document.getElementById("skippedColorlessBody"));
 
-    console.log('hey');
 
+
+}
+
+function displayPlayer(playerObject, playerClass) {
+    try {
+    document.getElementById(playerClass + "Names").innerHTML = playersToString(playerObject.Players);
+
+    setPlayerHealth(document.getElementById(playerClass + "HealthContainer"), playerObject.Health);
+    document.getElementById(playerClass + "GoldCount").innerHTML = playerObject.Gold;
+
+    //IRONCLAD
+    //Fille relic lists
+    fillItemList("relics", playerObject.Relics, document.getElementById(playerClass + "RelicsBody"));
+    fillItemList("boss_relics", playerObject.BossRelics, document.getElementById(playerClass + "BossRelicsBody"));
+    
+    //fill potions and cards
+    fillCardList("potions", playerObject.Potions, document.getElementById(playerClass + "PotionsBody"));
+    fillCardList(playerClass, playerObject.Deck, document.getElementById(playerClass + "DeckBody"));
+    fillCardList(playerClass, playerObject.RareDeck, document.getElementById(playerClass + "RareBody"));
+    fillCardList(playerClass, playerObject.RemovedCards, document.getElementById(playerClass + "RemovedBody"));
+    fillCardList(playerClass, playerObject.SkippedRares, document.getElementById(playerClass + "SkippedRareBody"));
+    } catch (error) {
+        console.log("Error parsing player! (" + playerClass + ") Are you sure the file is in the right format?");
+        console.log(error);
+    }
 }
 
 
@@ -116,28 +144,68 @@ function fillCardList(listName, cardList, cardContainer) {
 
     if (cardList.length != 0) {
         cardList.forEach(element => {
+            //convert a written name to the file name
             var lowerCaseName = element.toLowerCase();
             var convertedName = lowerCaseName.replace(/ /g, "_");
-            
+            //remove upgrade for file name
+            var extraClass = "";
+            if(convertedName.endsWith("+")){
+                convertedName = convertedName.substring(0, convertedName.length - 1);
+                extraClass = "upgraded-card-label";
+            }
+
+            //set image class for sizing
+            var imageClass = "card-image";
+            if(listName == "potions") {
+                imageClass = "potion-image";
+            }
+
+            //set file path
             var filePath = pathStart + convertedName + '.png';
 
-            console.log(filePath);
-
+            //Create html element
             var htmlString = '<li class="card-list-item ' + listName + '-card">'
-                            + '<span class="card-label">' + element + '</span>'
+                            + '<span class="card-label ' + extraClass + '">' + element + '</span>'
                             + '<div class="flex-spacer"></div>'
-                            + '<img class="card-image" src=' + filePath + '>';
-                            + '</li>';  
-                            
-                            
+                            + '<img class="' + imageClass + '" src=' + filePath + '>';
+                            + '</li>';                            
         
             cardContainer.innerHTML += htmlString;
             console.log(htmlString);
             console.log('hey');
         });
     } else {
-        itemContainer.innerHTML += '<span class="containerPlaceholder">None!</span>';
+        cardContainer.innerHTML += '<span class="containerPlaceholder">None!</span>';
     }
+}
+
+function playersToString(playerObject) {
+    var stringToReturn = "";
+
+    playerObject.forEach((element, index )=> {
+        if(index == 0) {
+            stringToReturn += element;
+        } else if (index > 0 && index < playerObject.length - 1) {
+            stringToReturn += ", " + element;
+        } else {
+            stringToReturn += " and " + element;
+        }
+
+        
+    console.log(stringToReturn);
+    });
+
+
+    return stringToReturn;
+}
+
+function setPlayerHealth(heartContainer, amount) {
+    var hearts = Array.from(heartContainer.children);
+
+    for(let i = 0; i < amount; i++ ) {
+        hearts[i].style.backgroundImage = "url('/static/images/heartFull.png')";
+    }
+        
 }
 
 function testBlob() { 
--- static/scripts/reader.js
+++ static/scripts/reader.js
@@ -9,7 +9,27 @@ function toggleActive(element) {
     element.classList.add("active")
 }
 
+function addHearts() {
+    const classNames = ['ironclad', 'silent', 'defect', 'watcher']
 
+    classNames.forEach(element => {
+        var healthContainer = document.getElementById(element + "HealthContainer");
+        var amount = 9;
+        if(element == "ironclad") { amount = 10; }
+
+        console.log(healthContainer);
+
+        for(let i = 1; i <= amount; i++) {
+            healthContainer.innerHTML += '<div class="heartContainer">' 
+                                    + '<img src="static/images/heartBroken.png" class="healthHeart">'
+                                    + '<label class="heartLabel">' + i + '</label>'
+                                    + '</div>'
+
+        }
+    });    
+}
+
+window.onload = addHearts();
 /*     "Ironclad": {
         "Players": [],
         "Health": 1,
